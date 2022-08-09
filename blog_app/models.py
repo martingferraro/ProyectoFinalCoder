8dd1b51c40948e7f73b626411beb1d702c481d8a
martingferraro <martingferraro@gmail.com>
2022-08-09 17:48:35 -0300

    commit 2

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Blog(models.Model):
     titulo= models.CharField(max_length=50)
     subtitulo= models.CharField(max_length=50)
-    cuerpo= models.TextField(max_length=500)
+    cuerpo= RichTextField(max_length=500)
     autor= models.CharField(max_length=50)
     fecha_creacion= models.DateField(auto_now_add=True)
     fecha_edicion= models.DateField(auto_now=True)
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog_app.views import home, blogformulario
 
 urlpatterns = [
-    path('home/', home, name="home"),
+    path('', home, name="home"),
     path('form/', blogformulario, name="form"),
+    path(r'ckeditor/', include('ckeditor_uploader.urls')),
+    
+    
     
 ]
